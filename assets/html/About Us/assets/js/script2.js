9d3f1b76dc608e67041437030fd9a07a8a866fee
Aditya Mali <avmali21-cse@bvucoep.edu.in>
2022-04-27 22:54:49 +0530

    Initial Summit

---
+++ assets/html/About Us/assets/js/script2.js
@@ -0,0 +1,89 @@
+var swiper = new Swiper('.slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+        invert: false,
+    },
+
+    pagination: {
+        el: '.slider-pagination',
+        clickable: true,
+    }
+});
+
+
+
+// function toggle() {
+//     var blur = document.getElementById("blur");
+//     blur.classList.toggle("active");
+
+//     var popup = document.getElementById("popup");
+//     popup.classList.toggle("active");
+//   }
+
+
+
+
+  function corporate() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_corporate");
+    popup.classList.toggle("active");
+  }
+
+
+  function tech() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_tech");
+    popup.classList.toggle("active");
+  }
+
+
+
+  function graphic() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_graphics");
+    popup.classList.toggle("active");
+  }
+
+
+  function marketing() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_marketing");
+    popup.classList.toggle("active");
+  }
+
+
+  function public() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_public");
+    popup.classList.toggle("active");
+  }
+
+
+  function research() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_research");
+    popup.classList.toggle("active");
+  }
+
+
+  function media() {
+    var blur = document.getElementById("blur");
+    blur.classList.toggle("active");
+
+    var popup = document.getElementById("popup_media");
+    popup.classList.toggle("active");
+  }
